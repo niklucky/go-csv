f734ff3557e127de92275eca8e46cdcef6697d6e
Nikita <iam@niklucky.com>
2017-08-23 00:54:23 +0300

    (feat) Added create method

--- main.go
+++ main.go
@@ -53,3 +53,45 @@ func (csvMapper *Mapper) Save(dir string, filename string, data [][]string, head
 	fmt.Println("CSV	| ", filename, "	| ", len(data), "\n==================================")
 	return nil
 }
+func (csvMapper *Mapper) Create(dir string, filename string, data [][]string, header []string) error {
+	var records [][]string
+	dest := csvMapper.Path + "/" + dir
+	_, err := os.Stat(dest)
+	if err != nil {
+		if os.IsNotExist(err) {
+			er := os.MkdirAll(dest, 0775)
+			if er != nil {
+				panic(er)
+			}
+		} else {
+			fmt.Println("Create directory:", err)
+		}
+	}
+	fname := dest + "/" + filename + ".csv"
+	_, err = os.Stat(fname)
+	if len(header) > 0 {
+		records = append(records, header)
+	}
+
+	if err != nil {
+		fmt.Println("Error stat:", err)
+	}
+	file, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, v := range data {
+		records = append(records, v)
+	}
+	e := writer.WriteAll(records)
+	if e != nil {
+		fmt.Println(e)
+	}
+	fmt.Println("CSV	| ", filename, "	| ", len(data), "\n==================================")
+	return nil
+}
